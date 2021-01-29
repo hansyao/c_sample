e603e5ae3156f3d5dcc93a4972feaadd48bd22cc
hansyao <hansyow@gmail.com>
2021-01-29 22:17:19 +0800

    the coding format change'

--- sample.c
+++ sample.c
@@ -2,14 +2,16 @@
 
 int a = -1;
 float i = 1.0, b, sum = 0;
-int main() {
-while (i <= 100) {
-	a = -a;
-	b = (1 / i) * a;
-	sum = sum + b;
-	i = i + 1;
-printf("count %f, sum is %f\n",i,sum);
+
+int main(int argc, char **argv) 
+{
+	while (i <= 100) {
+		a = -a;
+		b = (1 / i) * a;
+		sum = sum + b;
+		i = i + 1;
+		printf("count %f, sum is %f\n",i,sum);
 }
-//printf("total is %f\n", sum);
-return 0;
+	//printf("total is %f\n", sum);
+	return 0;
 }
