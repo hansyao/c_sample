f2023c8f0e5c50a09635400042107f574d542373
hansyao <hansyow@gmail.com>
2021-01-29 22:28:03 +0800

    update code format again

--- sample.c
+++ sample.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-int a = -1;
-float i = 1.0, b, sum = 0;
 
 int main(int argc, char **argv) 
-{
-	while (i <= 100) {
+
+	int a = -1;
+	float i = 1.0, b, sum = 0;
+
+	{
+	while (i <= 100) 
+		{
 		a = -a;
 		b = (1 / i) * a;
+
 		sum = sum + b;
 		i = i + 1;
-		printf("count %f, sum is %f\n",i,sum);
-}
+		printf("count %f, sum is %f\n", i, sum);
+	}
+
 	//printf("total is %f\n", sum);
 	return 0;
 }
