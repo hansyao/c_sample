99c0803664780e85955af9c16e1463f442542382
hansyao <hansyow@gmail.com>
2021-01-29 22:07:26 +0800

    update coding format

--- sample.c
+++ sample.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-int a=-1;
-float i=1.0,b,sum=0;
-int main(){
-while (i<=100){
-  a=-a;
-  b=(1/i)*a;
-  sum=sum+b;
-  i=i+1;
+
+int a = -1;
+float i = 1.0, b, sum = 0;
+int main() {
+while (i <= 100) {
+	a = -a;
+	b = (1 / i) * a;
+	sum = sum + b;
+	i = i + 1;
 printf("count %f, sum is %f\n",i,sum);
 }
 //printf("total is %f\n", sum);
