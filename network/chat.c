59e95b9b064200eba89c7b1ea34a75beb44748eb
Gavin Shan <gshan@redhat.com>
2021-06-13 20:21:06 +0800

    network/chat: Chatting program by TCP
    Signed-off-by: Gavin Shan <gshan@redhat.com>

---
+++ network/chat.c
@@ -0,0 +1,12 @@
+/*
+ * The program implements TCP server and client to fulfil the chatting
+ * function. The program supports the following options:
+ *
+ * ./chat -s
+ *   Running the program in server mode
+ * ./chat -n <nickname>
+ *   Running the program in client mode
+ *
+ */
+
+
