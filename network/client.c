9058075b16abd3b51f79bd6c3d47cba3a71531dd
hansyao <hansyow@gmail.com>
2021-06-20 18:47:51 +0800

    update pipe logic in parent process

--- network/client.c
+++ network/client.c
@@ -8,6 +8,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #include "chat.h"
 
@@ -15,10 +16,11 @@ int start_client(char *nickname)
 {
 	char SERVER_PORT[100];
 	char SERVER_IP[100];
-	int clientSocket, ret, fd[2];
+	int clientSocket, ret, fd[2], ret_fd;
 
 	char sendbuffer[1024], recvbuffer[1024];
-	char buf[100], state[64];
+	char buf[100];
+	int retp=pipe(fd); 
 
 	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket < 0) {
@@ -47,18 +49,17 @@ int start_client(char *nickname)
 	addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 	socklen_t addrlen = sizeof(addr);
 
-	ret = connect(clientSocket,(const struct sockaddr*)(&addr),addrlen);
+	ret = connect(clientSocket, (const struct sockaddr*)(&addr), addrlen);
 	if(ret == -1) {
 		perror("connect");
 		return -1;
 	}
+	
 	printf("connect server successfully!\n");
-	printf("Your nickname: %s\n",nickname);
+	printf("Your nickname: %s\n", nickname);
+	send(clientSocket, nickname, strlen(nickname)+1, 0);
 
-	send(clientSocket,nickname,strlen(nickname)+1,0);
-
-	int retp=pipe(fd); 
-	if(retp==-1) {
+	if(retp == -1) {
 		fprintf(stderr, "%s: Unable to create pipe\n", __func__);
 		return -ENOMEM;
 	}
@@ -72,21 +73,22 @@ int start_client(char *nickname)
     //send message
 	if(pid == 0) {
 		while(1) {
-            printf("%s: ",nickname);
-            fgets(sendbuffer,sizeof(sendbuffer),stdin);
-			if (strcmp(sendbuffer,COMMAND_QUIT) == 0) {
+            printf("%s: ", nickname);
+            fgets(sendbuffer, sizeof(sendbuffer), stdin);
+			if (strcmp(sendbuffer, COMMAND_QUIT) == 0) {
 				printf("do you wanna quit? (\"y\"->quit; any key continue) :");
 				fgets(buf, sizeof(buf), stdin);
 				if (strcmp(buf,"y\n") == 0){
-					/* define pipe val in parent pid */
+					/* define pipe val in parent process */
 					close(fd[0]);
 					char *state = COMMAND_QUIT;
-					write(fd[1],state,strlen(state));
-					wait(NULL);
+					write(fd[1], state, strlen(state));
+					waitpid(pid, NULL, 0);
+					close(fd[1]);
 					break;
 				}
 			}
-			if(send(clientSocket,sendbuffer,strlen(sendbuffer)+1,0)<=0) {
+			if(send(clientSocket, sendbuffer, strlen(sendbuffer)+1, 0) <= 0) {
 				break;	
 			}
 		}
@@ -94,21 +96,24 @@ int start_client(char *nickname)
 
     //receive message
     else{
+		/* read pipe status from child process */			
+		close(fd[1]);
+		ret_fd = read(fd[0], buf, sizeof(buf));
+		if( ret_fd > 0) {
+			if (strcmp(buf, COMMAND_QUIT) == 0) {
+				close(clientSocket);
+				return 0;
+			}
+		}
 		while(1) {
-			/* read pipe status from child pid */
-			close(fd[1]);
-			ret = read(fd[0],state,sizeof(state));
-			write(STDOUT_FILENO,state,ret);			//convert to standard output
-
-			if (strcmp(state,COMMAND_QUIT) == 0)
-				break;
-			if(recv(clientSocket,recvbuffer,1024,0)<=0)
+			if(recv(clientSocket, recvbuffer, sizeof(recvbuffer), 0) <= 0)
 				break;
 			time_t timep;
 			time(&timep);
-			printf("%s\n",ctime(&timep));
+			printf("%s <<<< %s\n", recvbuffer, ctime(&timep));
 		}
 	}
+
 	close(clientSocket);
 	return 0;	
 }
