72833be4b629dafef121a67493791b2e74335f22
hansyao <hansyow@gmail.com>
2021-06-21 10:36:30 +0800

    move structure definition to header

--- network/chat.h
+++ network/chat.h
@@ -1,6 +1,20 @@
+
 #define CHAT_CONFIG_FILE "config"
 #define COMMAND_QUIT "/quit\n"
+#define MAX 100
+
+typedef struct serverinfo {
+	char SERVER_IP[100];
+	char SERVER_PORT[100];
+} serverinfo;
+
+
+typedef struct Client {
+	int cfd;
+	char nickname[40];
+} Client;
 
 int chat_get_config(char *key, char *val);
-int start_server();
+int start_server(void);
 int start_client(char *nickname);
+
